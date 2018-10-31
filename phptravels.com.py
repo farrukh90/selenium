eba218a81cedb29e52862e505767bf858ddc2adc
farrukh90 <farrukhsadykov@gmail.com>
2018-10-31 11:06:49 -0500

    try added

--- phptravels.com.py
+++ phptravels.com.py
@@ -4,7 +4,14 @@ from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome()
 driver.set_page_load_timeout(10)
 driver.get("https://phptravels.org/clientarea.php")
-driver.find_element_by_xpath('//*[@id="inputEmail"]').send_keys("user@phptravels.com")
+try:
+    driver.find_element_by_xpath('//*[@id="inutEmail"]').send_keys("user@phptravels.com")
+except:
+    print("You are missing p")
+finally:
+    print("Moving on with the right one")
+    driver.find_element_by_xpath('//*[@id="inputEmail"]').send_keys("user@phptravels.com")
+
 driver.find_element_by_xpath('//*[@id="inputPassword"]').send_keys("demouser")
 driver.find_element_by_xpath('//*[@id="login"]').click()
 time.sleep(15)
