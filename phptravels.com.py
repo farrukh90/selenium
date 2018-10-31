5b8153da03cbd008eada7407fbf317fdea55858a
farrukh90 <farrukhsadykov@gmail.com>
2018-10-31 11:14:26 -0500

    chromedriver removed

--- phptravels.com.py
+++ phptravels.com.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
-driver = webdriver.Chrome()
+driver = webdriver.Chrome('/tmp/chromedriver')
 driver.set_page_load_timeout(10)
 driver.get("https://phptravels.org/clientarea.php")
 try:
